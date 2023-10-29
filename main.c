ed98da6e6054a1a19a0c0076c56316f92a000cb0
Antu Bertero <anturafaelbertero@gmial.com>
2023-10-29 19:35:26 -0300

    antu123

--- main.c
+++ main.c
@@ -10,6 +10,7 @@ int main()
     printf("hola soy antu!\n");
     printf("hola soy valen") ;
     printf("hola soy antuBertero") ;
+    printf("hola hola");
 >>>>>>> 571b0b5c1548b3ecf47a4301aecec3ba19541b40
     return 0;
 
